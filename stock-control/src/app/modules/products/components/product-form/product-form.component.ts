6f4f8262d9ab02e0fbc4a58252691e0dd3e3fdc0
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-02-06 12:07:39 -0300

    implementando funcionalidade de editar produto incluindo categoria

--- stock-control/src/app/modules/products/components/product-form/product-form.component.ts
+++ stock-control/src/app/modules/products/components/product-form/product-form.component.ts
@@ -7,10 +7,12 @@ import { Subject, take, takeUntil } from 'rxjs';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/getCategoriesResponse';
 import { CreateProductRequest } from 'src/app/models/interfaces/products/request/createProductRequest';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
-import { DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { EventAction } from 'src/app/models/interfaces/products/event/eventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/getAllProductsResponse';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
+import { ProductEvent } from 'src/app/models/enums/products/productEvent';
+import { EditProductRequest } from 'src/app/models/interfaces/products/request/editProductRequest';
 
 @Component({
   selector: 'app-product-form',
@@ -41,8 +43,16 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     name: ['', Validators.required],
     price: ['', Validators.required],
     description: ['', Validators.required],
-    amount: [0, Validators.required]
+    amount: [0, Validators.required],
+    category_id: ['', Validators.required]
   });
+
+  public saleProductSelected!: GetAllProductsResponse;
+  public renderDropdown = false;
+
+  public addProductAction = ProductEvent.ADD_PRODUCT_EVENT;
+  public editProductAction = ProductEvent.EDIT_PRODUCT_EVENT;
+  public saleProductAction = ProductEvent.SALE_PRODUCT_EVENT;
 
   constructor(
     private categoriesService: CategoriesService,
@@ -51,13 +61,19 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     private formBuilder: FormBuilder,
     private messageService: MessageService,
     private router: Router,
-    private ref: DynamicDialogConfig
+    private ref: DynamicDialogConfig,
+    private dynamicDialogref: DynamicDialogRef
   ) { }
 
   ngOnInit(): void {
     this.productAction = this.ref.data;
+
+    this.productAction?.event?.action === this.saleProductAction && this.getProductDatas();
+
     this.getAllCategories();
+    this.renderDropdown = true;
   }
+
   getAllCategories(): void {
     this.categoriesService.getAllCategories()
       .pipe(takeUntil(this.destroy$))
@@ -65,6 +81,10 @@ export class ProductFormComponent implements OnInit, OnDestroy {
         next: (response) => {
           if (response.length > 0) {
             this.categoriesDatas = response;
+
+            if (this.productAction?.event?.action === this.editProductAction && this.productAction?.productDatas) {
+              this.getProductSelectedDatas(this.productAction?.event?.id as string);
+            }
           }
         }
       })
@@ -108,8 +128,40 @@ export class ProductFormComponent implements OnInit, OnDestroy {
   }
 
   handleSubmitEditProduct(): void {
-    if (this.editProductForm.value && this.editProductForm.valid) {
+    if (this.editProductForm.value && this.editProductForm.valid && this.productAction.event.id) {
+      const requestEditProduct: EditProductRequest = {
+        name: this.editProductForm.value.name as string,
+        price: this.editProductForm.value.price as string,
+        description: this.editProductForm.value.description as string,
+        product_id: this.productAction?.event?.id,
+        amount: this.editProductForm.value.amount as number,
+        category_id: this.editProductForm.value.category_id as string
+      };
 
+      this.ProductsService
+        .editProduct(requestEditProduct)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: () => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Produto editado com sucesso',
+              life: 2500
+            });
+            this.dynamicDialogref.close();
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao editar produto',
+              life: 2500
+            });
+            this.dynamicDialogref.close();
+          }
+        })
     }
   }
 
@@ -128,7 +180,8 @@ export class ProductFormComponent implements OnInit, OnDestroy {
           name: this.productSelectedDatas?.name,
           price: this.productSelectedDatas?.price,
           amount: this.productSelectedDatas?.amount,
-          description: this.productSelectedDatas?.description
+          description: this.productSelectedDatas?.description,
+          category_id: this.productSelectedDatas?.category?.id
         });
       }
     }
