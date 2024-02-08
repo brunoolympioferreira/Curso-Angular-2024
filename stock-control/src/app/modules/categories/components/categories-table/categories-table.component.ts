e34d8196c8c2892332a9de05df4294f942ba3361
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-02-08 15:15:22 -0300

    implementa a funcionalidade de deletar categoria

--- stock-control/src/app/modules/categories/components/categories-table/categories-table.component.ts
+++ stock-control/src/app/modules/categories/components/categories-table/categories-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CategoryEvent } from 'src/app/models/enums/categories/categoryEvent';
+import { DeleteCategoryAction } from 'src/app/models/interfaces/categories/event/deleteCategoryAction';
+import { EditCategoryAction } from 'src/app/models/interfaces/categories/event/editCategoryAction';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/getCategoriesResponse';
 
 @Component({
@@ -8,7 +11,17 @@ import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/resp
 })
 export class CategoriesTableComponent {
   @Input() public categories: Array<GetCategoriesResponse> = []
+  @Output() public categoryEvent = new EventEmitter<EditCategoryAction>();
+  @Output() public deleteCategoryEvent = new EventEmitter<DeleteCategoryAction>();
 
   public categorySelected!: GetCategoriesResponse;
+  public addCategoryAction = CategoryEvent.ADD_CATEGORY_ACTION;
+  public editCategoryAction = CategoryEvent.EDIT_CATEGORY_ACTION;
+
+  handleDeleteCategoryEvent(category_id: string, categoryName: string): void {
+    if (category_id !== '' && categoryName !== '') {
+      this.deleteCategoryEvent.emit({ category_id, categoryName });
+    }
+  }
 
 }
