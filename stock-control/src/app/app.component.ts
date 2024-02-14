056fc085a20e1aa3cc08c05d6555ed8b581f5d34
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2024-02-14 08:44:43 -0300

    implementa funcionalidade de editar categoria

--- stock-control/src/app/app.component.ts
+++ stock-control/src/app/app.component.ts
@@ -13,5 +13,9 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.primeNfConfig.ripple = true;
+    this.primeNfConfig.setTranslation({
+      apply: 'Aplicar',
+      clear: 'Limpar'
+    })
   }
 }
